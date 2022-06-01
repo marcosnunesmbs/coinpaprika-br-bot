6776f2910ec13ea5d8ab12231bc172b741bddc5e
Marcos Nunes <marcosnunesmbs@gmail.com>
2022-06-01 00:30:16 -0300

    add express port dinamic

--- index.js
+++ index.js
@@ -181,7 +181,7 @@ app.use(express.json())
 app.get('/', function (req, res) {
     res.send('welcome to CPP-BR-BOT!');
 });
-
-app.listen(8080, function () {
-    console.log('Bot listening on port 5000!');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, function () {
+    console.log(`Our app is running on port ${ PORT }`);
 });
